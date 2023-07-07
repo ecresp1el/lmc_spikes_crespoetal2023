1d5727e7f027050e5f6a5e29e620f697522b0d85
Manny Crespo <cresp1el@cmich.edu>
2023-07-07 16:08:48 -0400

    updated to correctly load the data and save as a pickle file. Then made afunction to load the data in the notebook

--- get_path_script.py
+++ get_path_script.py
@@ -20,6 +20,10 @@ query = input("Please enter a string to query the .mat files: ")
 print("The file path is: " + file_path)
 print("The query string is: " + query)
 
+#intialize the empty dict outside of the for loop
+dict = {}
+
+
 #loop through the files in the file path that match the query string
 for file in os.listdir(file_path):
     #the file name should have the query string in it and if it does store in a csv file called the query string as the file name
@@ -31,20 +35,21 @@ for file in os.listdir(file_path):
             #find keys that are not '__header__', '__version__', '__globals__' and store them in a list called 'keys'
         keys = [key for key in mat.keys() if key not in ['__header__', '__version__', '__globals__']]#print the keys
 
-        #now create a dictionary dynamically that will store the file name as the key name the the variable mat[key] as the value
+        #now beging to add to the dictionary called dict that was previously intializd dynamically that will store the file name as the key name the the variable mat[key] as the value
+        #loop through the keys list and add the key and value to the dictionary
         for key in keys:
-            #create a dictionary dynamically where the file name without the .mat extension is the key and the value is the variable mat[key]
-            dict = {file[:-4]: mat[key]} #remove the .mat extension from the file name 
+            #remove the .mat from the file nams from when adding it to the dict 
+            dict[file.replace('.mat', '')] = mat[key]           
             
     #if not than say no files match the query string
     if not file:
         print("No files match the query string")
         
 #store the dictionary as pickle in the provided file path for later use outside of this script
+#prrint the keys of the dictionary dict to make sure it is correct
+print(dict.keys())
 import pickle
-pickle.dump(dict, open(file_path + '/' + query + '.p', 'wb'))
-
-
+pickle.dump(dict, open(file_path + '/' + query + '.p', 'wb'))   
 
 # Close the root window 
 root.quit()
