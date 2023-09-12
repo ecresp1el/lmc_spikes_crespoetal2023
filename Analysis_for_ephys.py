271025328b40f05eb3ff96e750bec280016971d3
ecresp1el <cresp1el@cmich.edu>
2023-09-12 13:39:31 -0400

    added the def calculate_and_plot_firing_rate_matrix(self, bin_size=1.0):

--- Analysis_for_ephys.py
+++ Analysis_for_ephys.py
@@ -1,4 +1,8 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 class UnitDataAnalysis:
     def __init__(self, unit_data):
@@ -24,6 +28,40 @@ class UnitDataAnalysis:
         if self.unit_data is None or not isinstance(self.unit_data, pd.DataFrame):
             raise ValueError("self.unit_data is not properly initialized")
         
+        
+        #perform the conversion and add the new columns to the DataFrame 
         self.unit_data['SpikeTimes_in_seconds'] = self.unit_data['SpikeTimes_all'] / self.unit_data['Sampling_Frequency']
         self.unit_data['ChemStimTime_in_seconds'] = self.unit_data['ChemStimTime_samples'] / self.unit_data['Sampling_Frequency']
+        
+    def calculate_and_plot_firing_rate_matrix(self, bin_size=1.0):
+        # Step 1: Determine the Bin Edges
+        total_duration = self.unit_data['Recording_Duration'].max()  # Assuming 'Recording_Duration' is in seconds
+        bin_edges = np.arange(0, total_duration, bin_size)
+        
+        # Step 2 & 3: Calculate Spike Counts and Firing Rates
+        num_units = len(self.unit_data)
+        firing_rate_matrix = np.full((num_units, len(bin_edges)-1), np.nan)  # Initialize with NaN values
+        
+        for i, (unit_id, unit_data) in enumerate(self.unit_data.iterrows()):
+            spike_times = unit_data['SpikeTimes_in_seconds']
+            recording_duration = unit_data['Recording_Duration']
+            
+            # Adjust the bin edges for the current unit based on its recording duration
+            unit_bin_edges = np.arange(0, recording_duration, bin_size)
+            
+            spike_counts, _ = np.histogram(spike_times, bins=unit_bin_edges)
+            firing_rate_matrix[i, :len(unit_bin_edges)-1] = spike_counts / bin_size
+        
+        # Step 4: Normalize the Firing Rates
+        max_firing_rates = np.nanmax(firing_rate_matrix, axis=1)
+        normalized_firing_rate_matrix = firing_rate_matrix / max_firing_rates[:, np.newaxis]
+        
+        # Step 5: Plot the Matrix
+        plt.imshow(normalized_firing_rate_matrix, aspect='auto', cmap='RdPu', vmax=1)
+        plt.colorbar(label='Normalized Firing Rate')
+        plt.xlabel('Time Bin')
+        plt.ylabel('Unit')
+        plt.title('Normalized Firing Rate Matrix')
+        plt.show()
+
         
